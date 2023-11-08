5e76bca7580348a1a514410c42362ddcdf210b70
STATION955\Analista19 <analista19.ti@connectsa.com.br>
2023-11-08 16:10:54 -0300

    atualizações - cors

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({origin: "*"}))
 
 function authenticateToken(req, res, next) {
     console.log(req.headers)
